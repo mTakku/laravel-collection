a52f375dc417e2a63dde9c1284456d2be82b8779
Takku <farelvivo06@gmail.com>
2024-04-06 01:48:32 +0700

    added lazycollection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -439,5 +439,22 @@ class CollectionTest extends TestCase
 
     }
 
+    public function testLazyCollection()
+    {
+
+        $collection = LazyCollection::make(function () {
+            $value = 0;
+
+            while (true) {
+                yield $value;
+                $value++;
+            }
+        });
+
+        $result = $collection->take(10);
+        $this->assertEqualsCanonicalizing([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], $result->all());
+
+    }
+
 }
 
