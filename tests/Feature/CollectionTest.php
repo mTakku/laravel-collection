53b065383961bb556fd4dd55b4a678ebfe42d76e
Takku <farelvivo06@gmail.com>
2024-04-06 01:14:11 +0700

    added filterindex

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -193,4 +193,15 @@ class CollectionTest extends TestCase
         ], $result->all());
 
     }
+
+    public function testFilterIndex()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        $result = $collection->filter(function ($value, $key) {
+            return $value % 2 == 0;
+        });
+
+        $this->assertEqualsCanonicalizing([2, 4, 6, 8, 10], $result->all());
+
+    }
 }
