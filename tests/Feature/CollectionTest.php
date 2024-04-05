938a9af55e9b008a0b09a6fd54c5e7c542729765
Takku <farelvivo06@gmail.com>
2024-04-06 00:36:53 +0700

    added manipulasi collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -22,4 +22,16 @@ class CollectionTest extends TestCase
         }
     }
 
+    public function testCrud()
+    {
+        $collection = collect([]);
+        $collection->push(1, 2, 3);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+
+        $result = $collection->pop();
+        $this->assertEquals(3, $result);
+        $this->assertEqualsCanonicalizing([1, 2], $collection->all());
+    }
+
+
 }
