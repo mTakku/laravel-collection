76f80fb986041f6c8749c73f84966d6a0c315771
Takku <farelvivo06@gmail.com>
2024-04-06 01:08:09 +0700

    added concat

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -111,4 +111,13 @@ class CollectionTest extends TestCase
             collect([3, 6]),
         ], $collection3->all());
     }
+
+    public function testConcat()
+    {
+        $collection1 = collect([1, 2, 3]);
+        $collection2 = collect([4, 5, 6]);
+        $collection3 = $collection1->concat($collection2);
+
+        $this->assertEqualsCanonicalizing([1, 2, 3, 4, 5, 6], $collection3->all());
+    }
 }
